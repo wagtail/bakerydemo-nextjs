a93c1bc185e27b2b537025f48986ba96baf96632
Sage Abdullah <sage.abdullah@torchbox.com>
2025-02-27 10:50:59 +0000

    Allow images from localhost

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'localhost',
+      },
+      {
+        hostname: '127.0.0.1',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
