3b044119953f7288853d99fa1b4258fce3113f91
Sage Abdullah <sage.abdullah@torchbox.com>
2025-06-03 09:31:59 +0100

    Use SameSite=None for draft mode cookie

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,4 +1,4 @@
-import { draftMode } from 'next/headers';
+import { cookies, draftMode } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 export async function GET(request: Request) {
@@ -14,5 +14,24 @@ export async function GET(request: Request) {
 
   const draft = await draftMode();
   draft.enable();
+
+  // Override draft mode cookie with SameSite=None and Secure attributes so it
+  // can be used in cross-origin requests (via the live preview panel iframe).
+  // https://github.com/vercel/next.js/issues/49927
+  const cookieStore = await cookies();
+  const prerenderBypass = cookieStore.get('__prerender_bypass')!;
+
+  // Additionally, set the Path so that the cookie is not applied to non-preview
+  // pages, and set the MaxAge so we don't hold onto the cookie for too long.
+  cookieStore.set({
+    name: '__prerender_bypass',
+    value: prerenderBypass.value,
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+    path: '/$preview',
+    maxAge: 60 * 60, // 1 hour
+  });
+
   return redirect(`/$preview?${searchParams.toString()}`);
 }
